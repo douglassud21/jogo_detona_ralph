c260d8828e42cad845b30181271efeee1f7ef246
Douglas <douglas.nascimentosud21@gmail.com>
2024-10-10 00:57:28 -0300

    Update engine.js

--- game/js/engine.js
+++ game/js/engine.js
@@ -9,7 +9,9 @@ const state = {
 
     values:{
         timerId: null,
-        gameVelocity: 1000
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
     },
 };
 
@@ -21,6 +23,7 @@ function randomSquare () {
     let randomNumber = Math.floor (Math.random () * 9);
     let randomSquare = state.view.squares [randomNumber];
     randomSquare.classList.add ("enemy");
+    state.values.hitPosition = randomSquare.id;
 }
 
 function moveEnemy () {
@@ -28,11 +31,20 @@ function moveEnemy () {
 }
 
 function addListenerHitBox () {
-    state.view.squares.forEach ((square) => {})
+    state.view.squares.forEach ((square) => {
+        square.addEventListener ("mousedown", () => {
+            if (square.id === state.values.hitPosition) {
+                state.values.result++
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+            }
+        })
+    })
 }
 
 function initialize () {
     moveEnemy ();
+    addListenerHitBox ();
 };
 
 initialize ();
